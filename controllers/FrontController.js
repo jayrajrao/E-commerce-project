359fc91df5eb26e0eb623c534178cb6b134a06a3
jayrajrao <108978622+jayrajrao@users.noreply.github.com>
2023-01-26 17:46:36 +0530

    first

---
+++ controllers/FrontController.js
@@ -0,0 +1,88 @@
+
+//const categorymodel = require("../Model/category")
+const AdminModel = require("../Model/admin");
+const AboutModel = require("../Model/about");
+const BlogModel = require("../Model/blog")
+
+class FrontController {
+  static home = async(req, res) => {
+    const data = await BlogModel.find()
+    //res.send('helo about')
+    //console.log(data)
+    res.render("home", {d:data});
+  };
+  static about = async(req, res) => {
+    const aboutdata =await AboutModel.find()
+    //res.send('helo about')
+    res.render("about", {ab:aboutdata});
+  };
+  static contact = (req, res) => {
+    //res.send('helo about')
+    res.render("contact");
+  };
+
+  static blog = (req, res) => {
+   // const bloglist = await BlogModel.find()
+    res.render("blog", );
+  };
+  static login = (req, res) => {
+    res.render("login");
+  };
+  static adminregister = async (req, res) => {
+    res.render("register", { message: req.flash("error") });
+  };
+  static admininsert = async (req, res) => {
+    try {
+      //console.log(req.body)
+      const { name, email, password, cpassword } = req.body;
+      const admin = await AdminModel.findOne({ email: email });
+      //console.log(admin)
+      if (admin) {
+        req.flash("error", "email already exists");
+        res.redirect("/register");
+      } else {
+        if (name && email && password && cpassword) {
+          if (password == cpassword) {
+            try {
+              const hashpassword = await bcrypt.hash(password,10)
+              const result = new AdminModel({
+                name: name,
+                email: email,
+                password: hashpassword,
+              });
+              await result.save();
+              req.flash("success", "registration sucessuful  Please login ");
+              res.redirect("/login");
+            } catch (err) {
+              console.log(err);
+            }
+          } else {
+            req.flash("error", "Password and confirm password doesnot match");
+            res.redirect("/register");
+          }
+        } else {
+          req.flash("error", "All Field are required");
+          res.redirect("/register");
+        }
+      }
+
+      //
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  static blogdetail= async(req,res)=>{
+    try {
+     // const category = await categorymodel.find()
+      const recentblog = await BlogModel.find()
+      const result = await BlogModel.findById(req.params.id)
+      //console.log(result)
+      res.render("blogdetail", {r:result, recentblog:recentblog, })
+    } catch (err) {
+      console.log(err)
+    }
+  }
+}
+
+module.exports = FrontController;
---
+++ index.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const AdminController = require("./controllers/admin/AdminController");
+const BlogController = require("./controllers/admin/BlogController");
+//const categorymodel = require("./Model/category")
+const CategoryController = require("./controllers/admin/CategoryController");
+const AboutController = require("./controllers/admin/AboutController")
+const { about } = require('./controllers/FrontController')
+const FrontController = require("./controllers/FrontController");
+
+const connectDB = require("./DB/connectdb");
+const index = express();
+const port = 3002;
+const bodyParser = require("body-parser");
+
+const fileupload = require("express-fileupload");
+index.use(fileupload({ useTempFiles: true }));
+
+const cloudinary = require("cloudinary");
+
+//mongoose connections
+connectDB();
+
+//setup ejs
+index.set("view engine", "ejs");
+
+//static files path
+index.use(express.static("Public"));
+
+//body-parser links
+index.use(bodyParser.urlencoded({ extended: false }));
+
+//frontend controllers
+index.get("/", FrontController.home);
+index.get("/about", FrontController.about);
+index.get("/contact", FrontController.contact);
+index.get("/blog", FrontController.blog);
+index.get("/login", FrontController.login);
+index.get("/blogdetail/:id", FrontController.blogdetail)
+index.get('/register',FrontController.adminregister)
+index.post('/adminregister',FrontController.admininsert)
+
+//admin controllers
+index.get("/admin/dashboard", AdminController.Dashboard);
+
+//admin blog controllers
+index.get("/admin/blogdisplay", BlogController.blogdisplay);
+index.post("/bloginsert", BlogController.bloginsert);
+index.get("/admin/blogview/:id", BlogController.blogview);
+index.get("/admin/blogedit/:id", BlogController.blogedit);
+index.post("/blogupdate/:id", BlogController.blogupdate);
+index.get("/admin/blogdelete/:id", BlogController.blogdelete);
+
+//category controllers
+//index.get('/admin/categorydisplay', CategoryController.categorydisplay)
+
+//admin about page 
+index.get('/admin/aboutus',AboutController.aboutdisplay)
+index.get('/admin/aboutedit/:id',AboutController.aboutedit)
+index.post('/aboutupdate/:id',AboutController.aboutupdate)
+
+
+index.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
